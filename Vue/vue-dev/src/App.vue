be4407d6b2bf2e5ae2b55b21f2f14c1a863cbad3
Gert Labuschagne <gertl@as-tech.co.za>
2020-02-20 19:53:25 +0200

    Continuing with Vue Complete Course

---
+++ Vue/vue-dev/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+<!--    <MyComponent></MyComponent>-->
+    <Section2></Section2>
+  </div>
+</template>
+
+<script>
+  //import HelloWorld from './components/HelloWorld.vue'
+  import Section2 from "./components/Section2";
+  //import Exercise2 from "./components/Exercise2";
+
+export default {
+  name: 'app',
+  components: {
+    //MyComponent,
+    Section2
+  }
+}
+</script>
+
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue/vue-dev/src/components/Section2.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="component">
+        <div id="test">
+            <button v-on:click="increase(2, $event)">Click me</button>
+            <button v-on:click="counter++">Click me</button>
+            <p>
+                Counter: {{ counter > 10 ? "Greater than 10" : "Smaller"}}
+            </p>
+            <p v-on:mousemove="updateCoord">
+                Coordinates: {{ x }} / {{ y }}
+                - <span v-on:mousemove.stop.prevent="">DEADSPOT</span>
+            </p>
+<!--            <input type="text" v-on:keyup.enter="alertMe">-->
+            <input type="text" v-on:keydown.enter="alertName" v-model="name">
+        </div>
+    </div>
+
+</template>
+
+<script>
+    var counter = 0;
+    var x = 0;
+    var y = 0;
+
+    var name = "AcidNuke";
+
+    export default {
+        name: "Section2",
+        data() {
+            return {
+                counter,
+                x,
+                y,
+                name
+            }
+        },
+        methods: {
+            // eslint-disable-next-line no-unused-vars
+            increase: function (amount, event) {
+                this.counter += amount;
+
+            },
+            updateCoord: function (event) {
+                this.x = event.clientX;
+                this.y = event.clientY;
+            },
+            alertMe: function () {
+                alert('Alert');
+            },
+            alertName: function () {
+                alert(name);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
